a1ee6de1b2385f1c7d187d6652ed14385709b29e
william da cunha <79292269+williamdcunha@users.noreply.github.com>
2023-09-22 12:23:25 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import './App.css';
+import Primeiro from './components/Primeiro.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1>
+        Meu Projeto React
+      </h1>
+      
+      <Primeiro />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Primeiro.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Primeiro = () => {
+  return (
+    <h1>
+        Olá Mundo!
+    </h1>
+  )
+}
+
+export default Primeiro
